cb8e607b0e1487e52fdbc6ae86ab9d11f3b6506b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to set the MaxLines property in TextEdit
+
+
+<p>Although TextEdit doesn't provide the MaxLines property, it is possible to set this property in the TextBox that is used by TextEdit to edit text. The inner TextBox can be obtained from the <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfEditorsBaseEdit_EditCoretopic"><u>EditCore</u></a> property.</p>
+
+<br/>
+
+
---
+++ VB/example/MainWindow.xaml.vb
@@ -0,0 +1,51 @@
+﻿Imports Microsoft.VisualBasic
+Imports DevExpress.Xpf.Editors
+Imports System
+Imports System.Collections.Generic
+Imports System.Collections.ObjectModel
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+
+Namespace example
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+
+
+	Public Class Helper
+
+        Public Shared ReadOnly MaxLinesProperty As DependencyProperty = DependencyProperty.RegisterAttached("MaxLines", GetType(Integer), GetType(Helper), New FrameworkPropertyMetadata(AddressOf MaxLinesPropertyChanged))
+		Public Shared Sub SetMaxLines(ByVal element As UIElement, ByVal value As Integer)
+			element.SetValue(MaxLinesProperty, value)
+		End Sub
+		Public Shared Function GetMaxLines(ByVal element As UIElement) As Integer
+			Return CInt(Fix(element.GetValue(MaxLinesProperty)))
+		End Function
+
+		Private Shared Sub MaxLinesPropertyChanged(ByVal source As DependencyObject, ByVal e As DependencyPropertyChangedEventArgs)
+			AddHandler (CType(source, TextEdit)).Loaded, AddressOf Helper_Loaded
+		End Sub
+
+		Private Shared Sub Helper_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim editor As TextEdit = TryCast(sender, TextEdit)
+			CType(editor.EditCore, TextBox).MaxLines = CInt(Fix(editor.GetValue(Helper.MaxLinesProperty)))
+		End Sub
+
+	End Class
+End Namespace
